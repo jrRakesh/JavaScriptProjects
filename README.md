16ca42afefe75104f56f827efddcb140b22c1972
Rakesh Mehta <reetsnp@gmail.com>
2025-09-20 12:36:53 +0545

    animating color in rectangle using gsap

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Live demos (GitHub Pages):
 - [ageCalculator](https://jrRakesh.github.io/JavaScriptProjects/ageCalculator/)
 - [jsQuiz](https://jrRakesh.github.io/JavaScriptProjects/jsQuiz/)
 - [Duggo_Fetch](https://jrRakesh.github.io/JavaScriptProjects/Duggo_Fetch/)
+- [Duggo_Fetch](https://jrRakesh.github.io/JavaScriptProjects/colorChangingRectangle/)
--- colorChangingRectangle/script.js
+++ colorChangingRectangle/script.js
@@ -1,11 +1,28 @@
 let rectangle = document.querySelector("#center");
-rectangle.addEventListener("mousemove", function(details){
-    let rectangleLocation = rectangle.getBoundingClientRect();
-    let insideRectangleValue = details.clientX - rectangleLocation.left;
+rectangle.addEventListener("mousemove", function (details) {
+  let rectangleLocation = rectangle.getBoundingClientRect();
+  let insideRectangleValue = details.clientX - rectangleLocation.left;
 
-    if(insideRectangleValue < rectangleLocation.width/2){
-        let redColor = gsap
-    } else {
-        console.log("right");
-    }
-})
+  if (insideRectangleValue < rectangleLocation.width / 2) {
+    let redColor = gsap.utils.mapRange(
+      0,
+      rectangleLocation.width / 2,
+      255,
+      0,
+      insideRectangleValue
+    );
+    gsap.to(rectangle, { backgroundColor: `rgb(${redColor}, 0, 0)` });
+  } else {
+    let blueColor = gsap.utils.mapRange(
+      rectangleLocation.width / 2,
+      rectangleLocation.width,
+      0,
+      255,
+      insideRectangleValue
+    );
+    gsap.to(rectangle, { backgroundColor: `rgb(0, 0, ${blueColor})` });
+  }
+});
+rectangle.addEventListener("mouseleave", function () {
+  gsap.to(rectangle, { backgroundColor: `rgba(255, 255, 255, 1)` });
+});
